bf71bceff2e1b13416396c56b4c348245d719d70
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-03 23:44:19 +0530

    Implemented the home product details page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-const ProductCard = () => {
+// eslint-disable-next-line react/prop-types
+const ProductCard = ({image}) => {
   return (
-    <div>
+    <div className="w-48 flex flex-col   hover:shadow-gray-600 shadow-lg hover:scale-101 transition-all duration-300  cursor-pointer mb-6">
       <img
-        src="https://th.bing.com/th/id/OIP.RHzRocDQ-VPOif5AmSbAeQHaKl?rs=1&pid=ImgDetMain"
-        alt=""
+        src={image}
+              alt="product-image"
+              className="w-full "
       />
+      <div className="flex flex-col p-2 mt-auto">
+        <p className="font-bold">Arrow Sport</p>
+        <p className="text-gray-700 text-sm">Men Solid Tshirt</p>
+        <p className="text-sm">Rs. 1124</p>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   return (
-    <div className="bg-blue-300 w-full h-full">
-      <div className=" bg-amber-200 grid sm:grid-cols-12  ">
-        <div className="sm:col-span-2 sm:block hidden  border-1 min-h-96">Filter</div>
-        <div className="sm:col-span-10  min-h-96">Products</div>
+    <div className="w-full h-full">
+      <div className="  grid sm:grid-cols-12  ">
+        <div className="sm:col-span-2 sm:block hidden   min-h-96">
+          Filter
+        </div>
+        <div className="sm:col-span-10 p-6 sm:grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 justify-items-center">
+          <ProductCard image="https://th.bing.com/th/id/OIP.RHzRocDQ-VPOif5AmSbAeQHaKl?rs=1&pid=ImgDetMain" />
+          <ProductCard image="https://www.cottonheritage.com/catImg/WAMHIRES/MC1082589_061422125108.png" />
+          <ProductCard image="https://www.cottonheritage.com/catImg/WAMLARGE/ou1964_120122165702.jpg" />
+          <ProductCard image="https://www.cottonheritage.com/catImg/WAMLARGE/mc1182_082924091509.jpg" />
+          <ProductCard image="https://www.cottonheritage.com/catImg/WAMSMALL/mc1040_082924094923.jpg" />
+          <ProductCard image="https://www.cottonheritage.com/catImg/WAMSMALL/MC1086_040924092749.jpg" />
+          <ProductCard image="https://www.cottonheritage.com/catImg/WAMLARGE/MC1220_052424103221.jpg" />
+        </div>
       </div>
     </div>
   );
